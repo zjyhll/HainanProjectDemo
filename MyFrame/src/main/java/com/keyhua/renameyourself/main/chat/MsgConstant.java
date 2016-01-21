6db1607e07d99b1037019d8ba6cc2190d3a858c1
zjyhll <1573739541@qq.com>
2016-01-21 11:25:18 +0800

    Rejected commit: Default

---
+++ MyFrame/src/main/java/com/keyhua/renameyourself/main/chat/MsgConstant.java
@@ -0,0 +1,28 @@
+package com.keyhua.renameyourself.main.chat;
+
+/**
+ * demo 消息常量定义.
+ *
+ * @author mhuang.
+ */
+public interface MsgConstant {
+    /**
+     * 媒体流类型.
+     */
+    int MSG_MEDIA_STATE = 1;
+
+    /**
+     * 媒体流已关闭.
+     */
+    int MSG_DISCONNECTED = 10;
+
+    /**
+     * 媒体流正在关闭.
+     */
+    int MSG_DISCONNECTING = 9;
+
+    /**
+     * 媒体异常.
+     */
+    int MSG_EXCEPTION = 20;
+}
